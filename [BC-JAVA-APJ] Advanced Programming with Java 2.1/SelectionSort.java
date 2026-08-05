06acca3a720e60817f8113e3cc36dd93dfcd2de7
viethung138 <hungnv06.ptit@gmail.com>
2026-08-05 17:36:00 +0700

    update2

--- [BC-JAVA-APJ] Advanced Programming with Java 2.1/SelectionSort.java
+++ [BC-JAVA-APJ] Advanced Programming with Java 2.1/SelectionSort.java
@@ -9,7 +9,7 @@ public class SelectionSort {
     static double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
 
     public static void selectionSort(double[] list) {
-        for (int i = 0; i < list.length; i++) {
+        for (int i = 0; i < list.length - 1; i++) {
             int currentMaxIndex = i;
             double currentMax = list[i];
             for (int j = i + 1; j < list.length; j++) {

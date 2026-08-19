68f27d23b437327c8bd7b5988577d9921d2a2f81
viethung138 <hungnv06.ptit@gmail.com>
2026-08-20 01:07:14 +0700

    CrawlSongExample

--- [BC-JAVA-APJ] Advanced Programming with Java 2.1/AccountExample.java
+++ [BC-JAVA-APJ] Advanced Programming with Java 2.1/AccountExample.java
@@ -2,17 +2,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AccountExample {
-    private static Pattern pattern;
-    private Matcher matcher;
-
     private final String ACCOUNT_REGEX = "^[_a-z0-9]{6,}$";
 
+    private final Pattern PATTERN = Pattern.compile(ACCOUNT_REGEX);
+
     public AccountExample() {
-        pattern = Pattern.compile(ACCOUNT_REGEX);
+
     }
 
-    public boolean validate(String regex) {
-        matcher = pattern.matcher(regex);
+    public boolean validate(String accountName) {
+        Matcher matcher = PATTERN.matcher(accountName);
         return matcher.matches();
     }
 }

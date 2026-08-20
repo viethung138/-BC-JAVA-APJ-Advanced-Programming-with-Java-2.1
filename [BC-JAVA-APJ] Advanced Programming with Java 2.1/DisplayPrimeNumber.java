7e9045a8fbcbb50ca14877403805ce3ae1e4b793
viethung138 <hungnv06.ptit@gmail.com>
2026-08-21 01:03:16 +0700

    DisplayPrimeNumber

--- [BC-JAVA-APJ] Advanced Programming with Java 2.1/DisplayPrimeNumber.java
+++ [BC-JAVA-APJ] Advanced Programming with Java 2.1/DisplayPrimeNumber.java
@@ -8,8 +8,20 @@ public class DisplayPrimeNumber {
         int count = 0;
         int N = 2;
         System.out.println(numbers + " Số nguyên tố đầu tiên là: ");
-        while (count < t) {
-            for (int i == 2; i <= )
+        while (count < numbers) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(N); i++) {
+                if (N % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(N + " ");
+                count++;
+            }
+            N++;
         }
+        sc.close();
     }
 }

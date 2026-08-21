ae3a1df226461d3ce8eda4f3b8eeb3558d0153f0
viethung138 <hungnv06.ptit@gmail.com>
2026-08-21 23:00:20 +0700

    FindMinElement

--- [BC-JAVA-APJ] Advanced Programming with Java 2.1/ConvertTemperature.java
+++ [BC-JAVA-APJ] Advanced Programming with Java 2.1/ConvertTemperature.java
@@ -12,34 +12,35 @@ public class ConvertTemperature {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double fahrenheit;
-        double celsius;
-        int choice;
+        try (Scanner sc = new Scanner(System.in)) {
+            double fahrenheit;
+            double celsius;
+            int choice;
 
-        do {
-            System.out.println("Menu:");
-            System.out.println("1. Fahrenheit to Celsius");
-            System.out.println("2. Celsius to Fahrenheit");
-            System.out.println("0. Exit");
-            System.out.print("Enter your choice: ");
-            choice = sc.nextInt();
+            do {
+                System.out.println("Menu:");
+                System.out.println("1. Fahrenheit to Celsius");
+                System.out.println("2. Celsius to Fahrenheit");
+                System.out.println("0. Exit");
+                System.out.print("Enter your choice: ");
+                choice = sc.nextInt();
 
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter fahrenheit: ");
-                    fahrenheit = sc.nextDouble();
-                    System.out.println("Fahrenheit to Celsius: " + fahrenheitToCelsius(fahrenheit));;
-                    break;
-                case 2:
-                    System.out.print("Enter Celsius: ");
-                    celsius = sc.nextDouble();
-                    System.out.println("Celsius to Fahrenheit: " + celsiusToFahrenheit(celsius));
-                    break;
-                case 0:
-                    System.exit(0);
-            }
-            
-        } while (choice != 0);
+                switch (choice) {
+                    case 1 -> {
+                        System.out.print("Enter fahrenheit: ");
+                        fahrenheit = sc.nextDouble();
+                        System.out.println("Fahrenheit to Celsius: " + fahrenheitToCelsius(fahrenheit));
+                    }
+                    case 2 -> {
+                        System.out.print("Enter Celsius: ");
+                        celsius = sc.nextDouble();
+                        System.out.println("Celsius to Fahrenheit: " + celsiusToFahrenheit(celsius));
+                    }
+                    case 0 ->
+                        System.exit(0);
+                }
+
+            } while (choice != 0);
+        }
     }
 }

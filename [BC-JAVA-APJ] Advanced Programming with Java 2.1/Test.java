9ee89442861069185954a1fef7ef47a74ec67513
viethung138 <hungnv06.ptit@gmail.com>
2026-09-19 19:42:55 +0700

    Test

---
+++ [BC-JAVA-APJ] Advanced Programming with Java 2.1/Test.java
@@ -0,0 +1,26 @@
+class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    public Student() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("abc");
+        s1.setClasses("a1");
+        
+        System.out.println("Cập nhật thông tin thành công!");
+    }
+}
